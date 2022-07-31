926ca073d957fc351b48aace87ce3746d21d33ce
Kwok Wai Tang <kwok.tang68@gmail.com>
2022-07-31 12:03:18 +0100

    Updated to generate an OpenAPI 3 specification

--- src/main/java/com/kwt/basicspringbootsetup/controller/McuRestApiController.java
+++ src/main/java/com/kwt/basicspringbootsetup/controller/McuRestApiController.java
@@ -2,6 +2,12 @@ package com.kwt.basicspringbootsetup.controller;
 
 import com.kwt.basicspringbootsetup.dto.MarvelStudioFilmDto;
 import com.kwt.basicspringbootsetup.service.McuMovieService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +23,11 @@ import java.util.Optional;
 /**
  * For the Open API / Swagger page...
  * http://localhost:8080/swagger-ui/index.html#/mcu-rest-api-controller
+ *
+ * Also useful...
+ * https://springdoc.org/#migrating-from-springfox
  */
+@Tag(name = "mcu-movies", description = "The MCU movies API")
 @RestController
 @RequestMapping("/mcu-movies")
 public class McuRestApiController {
@@ -29,6 +39,11 @@ public class McuRestApiController {
         this.mcuMovieService = Objects.requireNonNull(mcuMovieService, () -> "Missing an MCU movie service");
     }
 
+    @Operation(summary = "Get all MCU movies", tags = { "mcu-movies" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = MarvelStudioFilmDto.class)) }),
+            @ApiResponse(responseCode = "204", description = "No MCU movies available", content = @Content)
+    })
     @GetMapping("/all")
     public ResponseEntity<List<MarvelStudioFilmDto>> getListOfMcuMovies() {
         Optional<List<MarvelStudioFilmDto>> mcuMovies = mcuMovieService.getMcuMovies();
@@ -41,6 +56,11 @@ public class McuRestApiController {
         return new ResponseEntity<>(Collections.emptyList(), HttpStatus.NO_CONTENT);
     }
 
+    @Operation(summary = "Get all MCU movies by release year", tags = { "mcu-movies" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = MarvelStudioFilmDto.class)) }),
+            @ApiResponse(responseCode = "204", description = "No MCU movies available", content = @Content)
+    })
     @GetMapping("/by-release-year")
     public ResponseEntity<List<MarvelStudioFilmDto>> getMcuMoviesByReleaseYear() {
         Optional<List<MarvelStudioFilmDto>> mcuMoviesByReleaseYear = mcuMovieService.getMcuMoviesByReleaseYear();
@@ -53,6 +73,11 @@ public class McuRestApiController {
         return new ResponseEntity<>(Collections.emptyList(), HttpStatus.NO_CONTENT);
     }
 
+    @Operation(summary = "Get all MCU movies by chronological order", tags = { "mcu-movies" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation", content = { @Content(mediaType = "application/json", schema = @Schema(implementation = MarvelStudioFilmDto.class)) }),
+            @ApiResponse(responseCode = "204", description = "No MCU movies available", content = @Content)
+    })
     @GetMapping("/by-chronological-order")
     public ResponseEntity<List<MarvelStudioFilmDto>> getMcuMoviesByChronologicalOrder() {
         Optional<List<MarvelStudioFilmDto>> mcuMoviesByChronologicalOrder = mcuMovieService.getMcuMoviesByChronologicalOrder();
