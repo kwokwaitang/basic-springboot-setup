94409c4d20026c08c811c7a4cd6a8576c558f4e4
Kwok Wai Tang <kwok.tang68@gmail.com>
2022-08-06 14:59:29 +0100

    Using Lombok to set-up MarvelStudioFilmDto

--- src/main/java/com/kwt/basicspringbootsetup/dto/MarvelStudioFilmDto.java
+++ src/main/java/com/kwt/basicspringbootsetup/dto/MarvelStudioFilmDto.java
@@ -1,8 +1,12 @@
 package com.kwt.basicspringbootsetup.dto;
 
-/**
- * Data Transfer Object - used to convey the extract data to the front-end
- */
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MarvelStudioFilmDto {
 
     private Long id;
@@ -12,60 +16,4 @@ public class MarvelStudioFilmDto {
     private Integer releaseYear;
 
     private Integer chronologicalOrder;
-
-    public MarvelStudioFilmDto() {
-    }
-
-    public MarvelStudioFilmDto(Long id, String name, Integer releaseYear, Integer chronologicalOrder) {
-        this.id = id;
-        this.name = name;
-        this.releaseYear = releaseYear;
-        this.chronologicalOrder = chronologicalOrder;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getReleaseYear() {
-        return releaseYear;
-    }
-
-    public void setReleaseYear(Integer releaseYear) {
-        this.releaseYear = releaseYear;
-    }
-
-    public Integer getChronologicalOrder() {
-        return chronologicalOrder;
-    }
-
-    public void setChronologicalOrder(Integer chronologicalOrder) {
-        this.chronologicalOrder = chronologicalOrder;
-    }
-
-    public String getWorseMcuMovieSoFar() {
-        return "Thor : The Dark World";
-    }
-
-    @Override
-    public String toString() {
-        return "MarvelStudioFilmDto{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", releaseYear=" + releaseYear +
-                ", chronologicalOrder=" + chronologicalOrder +
-                '}';
-    }
 }
